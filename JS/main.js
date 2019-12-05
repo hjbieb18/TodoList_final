286cc5985183d654b1210f567f25c278337f9d8f
Holden <hjbieb18@smumn.edu>
2019-12-05 04:51:57 -0600

    button

--- JS/main.js
+++ JS/main.js
@@ -2,10 +2,28 @@ new Vue ({
     el: '#app',
     data: {
         message: 'Hello',
+        todoText: '',
         todos: [
             { title: 'Todo 1' },
             { title: 'Todo 2'}
         ],
-        showTodos: true
+        showTodos: true,
+        count: 1
+
+    },
+    methods: {
+        createTodo: function() {
+            var todoText = this.todoText.trim();
+            if(todoText){
+                this.todos.push({ title: todoText});
+                this.todoText= '';
+            }
+        },
+        clearTodo: function() {
+            this.todoText = '';
+        },
+        increaseCounter: function () {
+            this.count = this.count+1;
+        }
     }
 });
