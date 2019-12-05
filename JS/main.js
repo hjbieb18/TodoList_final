904d2be0987a8f74c864640c2fd9c66a45f1f9fd
Holden <hjbieb18@smumn.edu>
2019-12-05 05:22:22 -0600

    input field

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,25 @@
+var TodoComponent = Vue.extend({
+    data: function (){
+        return {
+            inEditMode: false
+        }
+    },
+    props: ['todo'],
+    template: '<div><span v-on:click="clicked" v-show="!inEditMode">{{ todo.title }}</span><input v-on:keyup.enter="saved" v-model="todo.title" v-show="inEditMode" /> </div>',
+    methods: {
+        clicked: function(){
+            this.inEditMode = true;
+        },
+        saved: function() {
+            this.inEditMode = false;
+        }
+    }
+});
+
+Vue.component('todo-component', TodoComponent);
+
+
+
 new Vue ({
     el: '#app',
     data: {
